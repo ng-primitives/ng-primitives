ba83b2765ba683147f77b606b54baba0caa60da8
Ashley Hunter <ahunter2@opentext.com>
2024-03-06 11:09:36 +0000

    pending schematic

--- packages/tools/src/generators/schematic/generator.ts
+++ packages/tools/src/generators/schematic/generator.ts
@@ -1,15 +1,11 @@
-import { addProjectConfiguration, formatFiles, generateFiles, Tree } from '@nx/devkit';
+import { formatFiles, generateFiles, Tree } from '@nx/devkit';
 import * as path from 'path';
+import { getPrimitiveSourceRoot } from '../../utils';
 import { SchematicGeneratorSchema } from './schema';
 
 export async function schematicGenerator(tree: Tree, options: SchematicGeneratorSchema) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
-  });
+  throw new Error('Schematic generator not implemented.');
+  const projectRoot = getPrimitiveSourceRoot(tree, options.name);
   generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
   await formatFiles(tree);
 }
