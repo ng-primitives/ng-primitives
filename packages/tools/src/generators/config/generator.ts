65931148533e9ce8ece02ba8d2e76073a06ce192
Ashley Hunter <ahunter2@opentext.com>
2024-03-08 22:37:29 +0000

    fixing generator

--- packages/tools/src/generators/config/generator.ts
+++ packages/tools/src/generators/config/generator.ts
@@ -1,10 +1,10 @@
 import { formatFiles, generateFiles, names, Tree } from '@nx/devkit';
 import * as path from 'path';
-import { addExportToIndex, getPrimitivePath } from '../../utils';
+import { addExportToIndex, getPrimitiveSourceRoot } from '../../utils';
 import { ConfigGeneratorSchema } from './schema';
 
 export async function configGenerator(tree: Tree, options: ConfigGeneratorSchema) {
-  const sourceRoot = getPrimitivePath(tree, options.primitive);
+  const sourceRoot = getPrimitiveSourceRoot(tree, options.primitive);
   generateFiles(tree, path.join(__dirname, 'files'), sourceRoot, {
     ...options,
     ...names(options.primitive),
