9d6e8ca38fb16d0af65dfea15ce087fcb246c087
Ashley Hunter <20795331+ashley-hunter@users.noreply.github.com>
2024-06-21 23:07:56 +0100

    adding missing license headers

--- packages/ng-primitives/schematics/init/compat.ts
+++ packages/ng-primitives/schematics/init/compat.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright © 2024 Angular Primitives.
+ * https://github.com/ng-primitives/ng-primitives
+ *
+ * This source code is licensed under the CC BY-ND 4.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import { convertNxGenerator } from '@nx/devkit';
 import initGenerator from './generator';
 
--- packages/ng-primitives/schematics/init/generator.ts
+++ packages/ng-primitives/schematics/init/generator.ts
@@ -1,5 +1,16 @@
+/**
+ * Copyright © 2024 Angular Primitives.
+ * https://github.com/ng-primitives/ng-primitives
+ *
+ * This source code is licensed under the CC BY-ND 4.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import { Tree, addDependenciesToPackageJson, installPackagesTask } from '@nx/devkit';
 
+/**
+ *
+ * @param tree
+ */
 export async function initGenerator(tree: Tree) {
   addDependenciesToPackageJson(
     tree,
