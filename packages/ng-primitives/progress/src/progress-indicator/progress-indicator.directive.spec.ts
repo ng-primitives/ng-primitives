fd62b8e12e3470caaecf7043884613cfbba70752
Ashley Hunter <20795331+ashley-hunter@users.noreply.github.com>
2024-06-26 22:43:55 +0100

    adding progress bar tests

--- packages/ng-primitives/progress/src/progress-indicator/progress-indicator.directive.spec.ts
+++ packages/ng-primitives/progress/src/progress-indicator/progress-indicator.directive.spec.ts
@@ -5,11 +5,73 @@
  * This source code is licensed under the CC BY-ND 4.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import { render } from '@testing-library/angular';
+import { NgpProgress } from '../progress/progress.directive';
 import { NgpProgressIndicator } from './progress-indicator.directive';
 
 describe('NgpProgressIndicator', () => {
-  it('should create an instance', () => {
-    const directive = new NgpProgressIndicator();
-    expect(directive).toBeTruthy();
+  it('should set the data-state attribute when the value is between min and max', async () => {
+    const container = await render(
+      `<div ngpProgress>
+        <div data-testid="indicator" ngpProgressIndicator></div>
+      </div>`,
+      {
+        imports: [NgpProgress, NgpProgressIndicator],
+      },
+    );
+
+    const indicator = container.getByTestId('indicator');
+    expect(indicator.getAttribute('data-state')).toBe('loading');
+  });
+
+  it('should set the data-state attribute when the value is equal to max', async () => {
+    const container = await render(
+      `<div ngpProgress [ngpProgressValue]="value">
+        <div data-testid="indicator" ngpProgressIndicator></div>
+      </div>`,
+      {
+        imports: [NgpProgress, NgpProgressIndicator],
+        componentProperties: {
+          value: 100,
+        },
+      },
+    );
+
+    const indicator = container.getByTestId('indicator');
+    expect(indicator.getAttribute('data-state')).toBe('complete');
+  });
+
+  it('should set the data-value attribute', async () => {
+    const container = await render(
+      `<div ngpProgress [ngpProgressValue]="value">
+        <div data-testid="indicator" ngpProgressIndicator></div>
+      </div>`,
+      {
+        imports: [NgpProgress, NgpProgressIndicator],
+        componentProperties: {
+          value: 50,
+        },
+      },
+    );
+
+    const indicator = container.getByTestId('indicator');
+    expect(indicator.getAttribute('data-value')).toBe('50');
+  });
+
+  it('should set the data-max attribute', async () => {
+    const container = await render(
+      `<div ngpProgress [ngpProgressMax]="max">
+        <div data-testid="indicator" ngpProgressIndicator></div>
+      </div>`,
+      {
+        imports: [NgpProgress, NgpProgressIndicator],
+        componentProperties: {
+          max: 200,
+        },
+      },
+    );
+
+    const indicator = container.getByTestId('indicator');
+    expect(indicator.getAttribute('data-max')).toBe('200');
   });
 });
--- packages/ng-primitives/progress/src/progress/progress.directive.spec.ts
+++ packages/ng-primitives/progress/src/progress/progress.directive.spec.ts
@@ -5,11 +5,61 @@
  * This source code is licensed under the CC BY-ND 4.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import { render } from '@testing-library/angular';
 import { NgpProgress } from './progress.directive';
 
 describe('NgpProgress', () => {
-  it('should create an instance', () => {
-    const directive = new NgpProgress();
-    expect(directive).toBeTruthy();
+  it('should set the aria attributes', async () => {
+    const container = await render(
+      `<div data-testid="progress" ngpProgress ngpProgressValue="50"></div>`,
+      { imports: [NgpProgress] },
+    );
+
+    const progress = container.getByTestId('progress');
+    expect(progress.getAttribute('role')).toBe('progressbar');
+    expect(progress.getAttribute('aria-valuemax')).toBe('100');
+    expect(progress.getAttribute('aria-valuemin')).toBe('0');
+    expect(progress.getAttribute('aria-valuenow')).toBe('50');
+    expect(progress.getAttribute('aria-valuetext')).toBe('50%');
+  });
+
+  it('should set the data-state attribute when value is between min and max', async () => {
+    const container = await render(
+      `<div data-testid="progress" ngpProgress ngpProgressValue="50"></div>`,
+      { imports: [NgpProgress] },
+    );
+
+    const progress = container.getByTestId('progress');
+    expect(progress.getAttribute('data-state')).toBe('loading');
+  });
+
+  it('should set the data-state attribute when value is equal to max', async () => {
+    const container = await render(
+      `<div data-testid="progress" ngpProgress ngpProgressValue="100"></div>`,
+      { imports: [NgpProgress] },
+    );
+
+    const progress = container.getByTestId('progress');
+    expect(progress.getAttribute('data-state')).toBe('complete');
+  });
+
+  it('should set the data-value attribute', async () => {
+    const container = await render(
+      `<div data-testid="progress" ngpProgress ngpProgressValue="50"></div>`,
+      { imports: [NgpProgress] },
+    );
+
+    const progress = container.getByTestId('progress');
+    expect(progress.getAttribute('data-value')).toBe('50');
+  });
+
+  it('should set the data-max attribute', async () => {
+    const container = await render(
+      `<div data-testid="progress" ngpProgress ngpProgressMax="200"></div>`,
+      { imports: [NgpProgress] },
+    );
+
+    const progress = container.getByTestId('progress');
+    expect(progress.getAttribute('data-max')).toBe('200');
   });
 });
