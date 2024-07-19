c5a3329733b560028bf0c722ce17f71804bf5393
Ashley Hunter <20795331+ashley-hunter@users.noreply.github.com>
2024-07-19 23:14:08 +0100

    Update accordion-trigger.directive.ts

--- packages/ng-primitives/accordion/src/accordion-trigger/accordion-trigger.directive.ts
+++ packages/ng-primitives/accordion/src/accordion-trigger/accordion-trigger.directive.ts
@@ -6,8 +6,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 import { Directive, HostListener, input } from '@angular/core';
-import { uniqueId } from 'ng-primitives/utils';
 import { NgpFocusVisible, NgpHover, NgpPress } from 'ng-primitives/interactions';
+import { uniqueId } from 'ng-primitives/utils';
 import { injectAccordionItem } from '../accordion-item/accordion-item.token';
 import { injectAccordion } from '../accordion/accordion.token';
 import { NgpAccordionTriggerToken } from './accordion-trigger.token';
