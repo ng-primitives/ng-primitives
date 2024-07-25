e09114a705db836c448252ccb6fd2ec2ccce1195
Ashley Hunter <20795331+ashley-hunter@users.noreply.github.com>
2024-07-25 21:37:54 +0100

    chore: updating license headers

--- packages/ng-primitives/resize/src/resize/resize.directive.ts
+++ packages/ng-primitives/resize/src/resize/resize.directive.ts
@@ -2,7 +2,7 @@
  * Copyright © 2024 Angular Primitives.
  * https://github.com/ng-primitives/ng-primitives
  *
- * This source code is licensed under the CC BY-ND 4.0 license found in the
+ * This source code is licensed under the Apache 2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 import { DestroyRef, Directive, ElementRef, NgZone, OnInit, inject, output } from '@angular/core';
