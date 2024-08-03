81bcbdfcd3fad1834380ea93da96793a7b01de4c
benjaminforras <rsnconfigs@gmail.com>
2024-08-04 01:59:15 +0200

    docs(dark-mode): add dark mode to focus example

--- apps/documentation/src/app/examples/focus/focus.example.ts
+++ apps/documentation/src/app/examples/focus/focus.example.ts
@@ -20,20 +20,20 @@ import { NgpFocus } from 'ng-primitives/interactions';
       padding: 0 12px;
       border-radius: 0.5rem;
       box-shadow:
-        0 1px 2px 0 rgba(0, 0, 0, 0.05),
-        0 0 0 1px rgba(0, 0, 0, 0.1);
+        0 1px 2px 0 light-dark(rgba(0, 0, 0, 0.05), rgba(255, 255, 255, 0.007)),
+        0 0 0 1px light-dark(rgba(0, 0, 0, 0.1), rgba(255, 255, 255, 0.1));
       outline: none;
     }
 
     input[data-focus='true'] {
       box-shadow:
-        0 1px 2px 0 rgba(0, 0, 0, 0.05),
+        0 1px 2px 0 light-dark(rgba(0, 0, 0, 0.05), rgba(255, 255, 255, 0.007)),
         0 0 0 2px rgb(59 130 246);
     }
 
     p {
       font-size: 0.75rem;
-      color: #666;
+      color: light-dark(#666, #7e7e7e);
       margin-top: 0.25rem;
     }
   `,
