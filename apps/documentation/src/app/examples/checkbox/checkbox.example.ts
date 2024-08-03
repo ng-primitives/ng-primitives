745d8f7ce86f9d510abd67b78966bddc5153ed8e
benjaminforras <rsnconfigs@gmail.com>
2024-08-04 01:12:27 +0200

    docs(dark-mode): add dark mode to checkbox example

--- apps/documentation/src/app/examples/checkbox/checkbox.example.ts
+++ apps/documentation/src/app/examples/checkbox/checkbox.example.ts
@@ -9,6 +9,28 @@ import { NgpCheckbox } from 'ng-primitives/checkbox';
   imports: [NgIcon, NgpCheckbox],
   viewProviders: [provideIcons({ heroCheckMini })],
   styles: `
+    :host {
+      --checkbox-background-color: rgb(255 255 255);
+      --checkbox-border-color: rgb(229 229 229);
+      --checkbox-box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
+      --checkbox-background-hover: rgb(250 250 250);
+      --checkbox-border-checked: rgb(10 10 10);
+      --checkbox-background-checked: rgb(10 10 10);
+      --checkbox-box-shadow-focus: 0 1px 2px rgba(0, 0, 0, 0.05), 0 0 0 2px rgb(255, 255, 255),
+        0 0 0 4px rgb(59 130 246);
+      --checkbox-icon-color: rgb(255 255 255);
+
+      --checkbox-background-color-dark: rgb(43 43 43);
+      --checkbox-border-color-dark: rgb(128 128 128);
+      --checkbox-box-shadow-dark: 0 1px 2px rgba(255, 255, 255, 0.9);
+      --checkbox-background-hover-dark: rgb(43 43 43);
+      --checkbox-border-checked-dark: rgb(255 255 255);
+      --checkbox-background-checked-dark: rgb(255 255 255);
+      --checkbox-box-shadow-focus-dark: 0 1px 2px rgba(255, 255, 255, 0.9),
+        0 0 0 2px rgb(59 130 246), 0 0 0 4px rgb(59 130 246);
+      --checkbox-icon-color-dark: rgb(43 43 43);
+    }
+
     [ngpCheckbox] {
       display: flex;
       width: 1.25rem;
@@ -17,32 +39,41 @@ import { NgpCheckbox } from 'ng-primitives/checkbox';
       align-items: center;
       justify-content: center;
       border-radius: 0.25rem;
-      border: 1px solid rgb(229 229 229);
-      background-color: rgb(255 255 255);
-      box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
+      border: 1px solid light-dark(var(--checkbox-border-color), var(--checkbox-border-color-dark));
+      background-color: light-dark(
+        var(--checkbox-background-color),
+        var(--checkbox-background-color-dark)
+      );
+      box-shadow: var(--checkbox-box-shadow);
       padding: 0;
       outline: none;
       flex: none;
     }
 
     [ngpCheckbox][data-hover='true'] {
-      background-color: rgb(250 250 250);
+      background-color: light-dark(
+        var(--checkbox-background-hover),
+        var(--checkbox-background-hover-dark)
+      );
     }
 
     [ngpCheckbox][data-checked='true'] {
-      border-color: rgb(10 10 10);
-      background-color: rgb(10 10 10);
+      border-color: light-dark(var(--checkbox-border-checked), var(--checkbox-border-checked-dark));
+      background-color: light-dark(
+        var(--checkbox-background-checked),
+        var(--checkbox-background-checked-dark)
+      );
     }
 
     [ngpCheckbox][data-focus-visible='true'] {
-      box-shadow:
-        0 1px 2px rgba(0, 0, 0, 0.05),
-        0 0 0 2px rgb(255, 255, 255),
-        0 0 0 4px rgb(59 130 246);
+      box-shadow: light-dark(
+        var(--checkbox-box-shadow-focus),
+        var(--checkbox-box-shadow-focus-dark)
+      );
     }
 
     ng-icon {
-      color: rgb(255 255 255);
+      color: light-dark(var(--checkbox-icon-color), var(--checkbox-icon-color-dark));
       font-size: 0.75rem;
     }
   `,
