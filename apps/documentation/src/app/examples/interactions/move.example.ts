ab968d5724a0c7b2ff1011322fd7c18ac71500e7
benjaminforras <rsnconfigs@gmail.com>
2024-08-04 11:10:51 +0200

    docs(dark-mode): add dark mode to press and move examples

--- apps/documentation/src/app/examples/interactions/move.example.ts
+++ apps/documentation/src/app/examples/interactions/move.example.ts
@@ -15,14 +15,14 @@ import { NgpMoveEvent } from 'packages/ng-primitives/interactions/src/move/move.
     div {
       padding: 1rem;
       border-radius: 0.5rem;
-      color: rgb(10 10 10);
+      color: light-dark(rgb(10 10 10), rgb(255 255 255));
       border: none;
       font-weight: 500;
-      background-color: #fff;
+      background-color: light-dark(#fff, #18181b);
       box-shadow:
-        0 1px 3px 0 rgb(0 0 0 / 0.1),
-        0 1px 2px -1px rgb(0 0 0 / 0.1),
-        0 0 0 1px rgb(0 0 0 / 0.05);
+        0 1px 3px 0 light-dark(rgba(0, 0, 0, 0.1), rgba(255, 255, 255, 0.007)),
+        0 1px 2px -1px light-dark(rgba(0, 0, 0, 0.1), rgba(255, 255, 255, 0.1)),
+        0 0 0 1px light-dark(rgba(0, 0, 0, 0.05), rgba(255, 255, 255, 0.007));
       cursor: move;
       user-select: none;
       touch-action: none;
@@ -37,15 +37,15 @@ import { NgpMoveEvent } from 'packages/ng-primitives/interactions/src/move/move.
 
     div:focus-visible {
       box-shadow:
-        0 0 0 2px #f5f5f5,
+        0 0 0 2px light-dark(#f5f5f5, #3f3f46),
         0 0 0 4px rgb(59 130 246);
     }
 
     div[data-move='true'] {
       box-shadow:
-        0 1px 3px 2px rgb(0 0 0 / 0.1),
-        0 1px 2px 2px rgb(0 0 0 / 0.1),
-        0 0 0 1px rgb(0 0 0 / 0.05);
+        0 1px 3px 2px light-dark(rgba(0, 0, 0, 0.1), rgba(168, 168, 168, 0.007)),
+        0 1px 2px 2px light-dark(rgba(0, 0, 0, 0.1), rgba(168, 168, 168, 0.1)),
+        0 0 0 1px light-dark(rgba(0, 0, 0, 0.05), rgba(168, 168, 168, 0.007));
     }
   `,
 })
