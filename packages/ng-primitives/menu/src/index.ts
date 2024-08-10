85126ed478176a1454617e93a47087e4ad206d25
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2024-08-10 21:52:59 +0100

    feat(menu): menu primitive (#33)

---
+++ packages/ng-primitives/menu/src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Copyright © 2024 Angular Primitives.
+ * https://github.com/ng-primitives/ng-primitives
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export { NgpMenu } from './menu/menu.directive';
+export { NgpMenuToken } from './menu/menu.token';
+export { NgpMenuTrigger } from './menu-trigger/menu-trigger.directive';
+export { NgpMenuTriggerToken } from './menu-trigger/menu-trigger.token';
+export { NgpMenuItem } from './menu-item/menu-item.directive';
+export { NgpMenuItemToken } from './menu-item/menu-item.token';
