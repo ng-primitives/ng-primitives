586db79350190a0f837363bbb4b403656f3361f3
Ashley Hunter <20795331+ashley-hunter@users.noreply.github.com>
2024-08-26 22:42:11 +0100

    Update index.ts

--- packages/ng-primitives/autocomplete/src/index.ts
+++ packages/ng-primitives/autocomplete/src/index.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright © 2024 Angular Primitives.
+ * https://github.com/ng-primitives/ng-primitives
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 export { NgpAutocomplete } from './autocomplete/autocomplete.directive';
 export { NgpAutocompleteToken } from './autocomplete/autocomplete.token';
 export { NgpAutocompleteTrigger } from './autocomplete-trigger/autocomplete-trigger.directive';
