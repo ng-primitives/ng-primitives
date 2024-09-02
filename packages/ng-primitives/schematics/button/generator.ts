85d20f55a3d4ef495c65eb6261685d747323c264
Ashley Hunter <20795331+ashley-hunter@users.noreply.github.com>
2024-09-02 22:20:08 +0100

    fixing linting

--- packages/ng-primitives/schematics/button/generator.ts
+++ packages/ng-primitives/schematics/button/generator.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright © 2024 Angular Primitives.
+ * https://github.com/ng-primitives/ng-primitives
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { formatFiles, generateFiles, names, Tree } from '@nx/devkit';
 import * as path from 'path';
 import { ButtonGeneratorSchema } from './schema';
