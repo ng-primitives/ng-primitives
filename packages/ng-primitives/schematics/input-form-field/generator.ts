ff7865588493c89aa8d8f93a57c5278bf112bfcf
kedevked <edoh.dev@gmail.com>
2024-09-03 19:51:47 +0200

    feat: add input-form-field generator

---
+++ packages/ng-primitives/schematics/input-form-field/generator.ts
@@ -0,0 +1,27 @@
+/**
+ * Copyright © 2024 Angular Primitives.
+ * https://github.com/ng-primitives/ng-primitives
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import { formatFiles, generateFiles, names, Tree } from '@nx/devkit';
+import * as path from 'path';
+import { ButtonGeneratorSchema } from './schema';
+
+export async function inputFormFieldGenerator(tree: Tree, options: ButtonGeneratorSchema) {
+  generateFiles(tree, path.join(__dirname, 'files'), options.directory, {
+    ...options,
+    names: names(options.name),
+  });
+  await formatFiles(tree);
+
+  if (options.inlineStyle) {
+    tree.delete(path.join(options.directory, `${options.name}.component.${options.style}`));
+  }
+  if (options.inlineTemplate) {
+    tree.delete(path.join(options.directory, `${options.name}.component.html`));
+  }
+}
+
+export default inputFormFieldGenerator;
