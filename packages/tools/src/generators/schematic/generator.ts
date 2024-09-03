3f2be28666bbe11ad150f0a996a27d7699d5a283
Ashley Hunter <20795331+ashley-hunter@users.noreply.github.com>
2024-09-03 21:35:08 +0100

    feat: schematic generator

--- packages/tools/src/generators/schematic/generator.ts
+++ packages/tools/src/generators/schematic/generator.ts
@@ -1,11 +1,42 @@
-import { formatFiles, generateFiles, Tree } from '@nx/devkit';
+import { formatFiles, generateFiles, joinPathFragments, names, Tree, updateJson } from '@nx/devkit';
+import { generatorGenerator } from '@nx/plugin/generators';
 import * as path from 'path';
 import { getPrimitiveSourceRoot } from '../../utils';
 import { SchematicGeneratorSchema } from './schema';
 
 export async function schematicGenerator(tree: Tree, options: SchematicGeneratorSchema) {
   const projectRoot = getPrimitiveSourceRoot(tree, options.name);
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  const { fileName } = names(options.name);
+
+  await generatorGenerator(tree, {
+    name: options.name,
+    unitTestRunner: 'none',
+    description: `Add a ${options.name} component to the project.`,
+    directory: joinPathFragments(projectRoot, 'schematics', fileName),
+    nameAndDirectoryFormat: 'as-provided',
+    skipFormat: true,
+  });
+
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, {
+    ...options,
+    ...names(options.name),
+  });
+
+  updateJson(tree, path.join(projectRoot, 'generators.json'), json => {
+    json.generators[fileName] = {
+      factory: `./schematics/${fileName}/generator`,
+      schema: `./schematics/${fileName}/schema.json`,
+      description: `Add a ${options.name} component to the project.`,
+    };
+
+    json.schematics[fileName] = {
+      factory: `./schematics/${fileName}/compat`,
+      schema: `./schematics/${fileName}/schema.json`,
+      description: `Add a ${options.name} component to the project.`,
+    };
+    return json;
+  });
+
   await formatFiles(tree);
 }
 
