742498f39bc42f676f8042a53878808567b4ab4d
kedevked <edoh.dev@gmail.com>
2024-09-15 16:25:53 +0200

    feat: generate radio-item

--- packages/ng-primitives/schematics/radio/generator.ts
+++ packages/ng-primitives/schematics/radio/generator.ts
@@ -9,23 +9,59 @@ import { formatFiles, generateFiles, names, Tree } from '@nx/devkit';
 import * as path from 'path';
 import { RadioGeneratorSchema } from './schema';
 
+const radioItemStyles = `
+:host {}
+
+:host[data-hover="true"] {}
+
+:host[data-focus-visible="true"] {}
+
+:host[data-press="true"] {}
+
+:host[data-checked="true"] {}
+
+[ngpRadioIndicator] {}
+
+:host[data-checked="true"] [ngpRadioIndicator] {}
+
+.indicator-dot {}
+
+:host[data-checked="true"] .indicator-dot {}
+
+p {}
+
+:host[data-checked="true"] p {}`;
+const group = 'group';
+const item = 'item';
+const radioItemPath = 'radio-item';
+const radioGroupPath = 'radio-group';
 export async function radioGenerator(tree: Tree, options: RadioGeneratorSchema) {
   const { fileName } = names(options.name);
 
-  generateFiles(tree, path.join(__dirname, 'files'), options.directory, {
+  generateFiles(tree, path.join(__dirname, `files/${radioGroupPath}`), options.directory, {
     ...options,
-    ...names(options.name),
-    selector: `${options.prefix}-${fileName}`,
+    ...names(`${options.name}-${group}`),
+    selector: `${options.prefix}-${fileName}-${group}`,
     styles: `:host { }`,
     template: `<ng-content />`,
   });
 
+  generateFiles(tree, path.join(__dirname, `files/${radioItemPath}`), options.directory, {
+    ...options,
+    ...names(`${options.name}-${item}`),
+    selector: `${options.prefix}-${fileName}-${item}`,
+    styles: radioItemStyles,
+    template: `<ng-content />`,
+  });
+
   if (options.inlineStyle) {
-    tree.delete(path.join(options.directory, `${fileName}.component.${options.style}`));
+    tree.delete(path.join(options.directory, `${fileName}-${item}.component.${options.style}`));
+    tree.delete(path.join(options.directory, `${fileName}-${group}.component.${options.style}`));
   }
 
   if (options.inlineTemplate) {
-    tree.delete(path.join(options.directory, `${fileName}.component.html`));
+    tree.delete(path.join(options.directory, `${fileName}-${item}.component.html`));
+    tree.delete(path.join(options.directory, `${fileName}-${group}.component.html`));
   }
 
   await formatFiles(tree);
