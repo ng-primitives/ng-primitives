be70982ab7f13cab6cf70de49613eda923d87efc
Ashley Hunter <20795331+ashley-hunter@users.noreply.github.com>
2024-09-26 21:21:00 +0100

    refactor(interactions): extracting interactions into functions

--- packages/ng-primitives/internal/src/interactions/focus-visible.ts
+++ packages/ng-primitives/internal/src/interactions/focus-visible.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright © 2024 Angular Primitives.
+ * https://github.com/ng-primitives/ng-primitives
+ *
+ * This source code is licensed under the Apache 2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { FocusMonitor, FocusOrigin } from '@angular/cdk/a11y';
 import { effect, ElementRef, inject, Signal, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
