7702ffa8e8311f35d395c2eebbe8fbfd8711ba13
Ashley Hunter <20795331+ashley-hunter@users.noreply.github.com>
2024-11-29 23:17:32 +0000

    build: updating nx

---
+++ eslint.config.cjs
@@ -0,0 +1,50 @@
+const nx = require('@nx/eslint-plugin');
+
+module.exports = [
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  {
+    files: ['**/*.json'],
+    // Override or add rules here
+    rules: {},
+    languageOptions: {
+      parser: require('jsonc-eslint-parser'),
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@angular-eslint/no-host-metadata-property': 'off',
+      '@nx/workspace-prefer-entrypoint-imports': 'error',
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {},
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    rules: {},
+  },
+  {
+    files: ['**/*.component.html'],
+    rules: {
+      '@angular-eslint/template/prefer-self-closing-tags': ['error'],
+    },
+  },
+];
