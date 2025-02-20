010865df1b444c200a9a2414e7e878e819bccf6e
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2025-02-20 21:16:21 +0000

    docs: fixing build

--- apps/documentation/vite.config.ts
+++ apps/documentation/vite.config.ts
@@ -41,7 +41,7 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [
       analog({
-        ssr: true,
+        static: true,
         prerender: {
           routes: async () => [
             '/',
