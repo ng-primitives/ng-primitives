dcac009062fc033144545a94e9064b3eb3818bef
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2025-04-08 00:02:23 +0200

    Migrate state system into new project (#162)
    * feat(state): add new project
    
    * feat(state): migrate ng-primitives/state => ng-primitives-state/state
    
    * refactor: post PR comment
    
    * fixing tsconfig path
    
    * fix: copying state from ng-primitives
    
    * removing unused dep
    
    ---------
    
    Co-authored-by: Ashley Hunter <ashley.hunter@hotmail.co.uk>

--- packages/ng-primitives/state/src/index.ts
+++ packages/ng-primitives/state/src/index.ts
@@ -1,1 +1,117 @@
-export * from './state';
+import {
+  computed,
+  FactoryProvider,
+  inject,
+  InjectionToken,
+  InputSignal,
+  InputSignalWithTransform,
+  isSignal,
+  linkedSignal,
+  ProviderToken,
+  signal,
+  Signal,
+  WritableSignal,
+} from '@angular/core';
+
+/**
+ * This converts the state object to a writable state object.
+ * This means that inputs become signals which are writable.
+ */
+export type State<T> = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [K in keyof T]: T[K] extends InputSignalWithTransform<infer U, any>
+    ? WritableSignal<U>
+    : T[K] extends InputSignal<infer R>
+      ? WritableSignal<R>
+      : T[K];
+};
+
+export type InjectedState<T> = Signal<State<T>>;
+
+/**
+ * Create a new injection token for the state.
+ * @param description The description of the token
+ */
+export function createStateToken<T>(description: string): InjectionToken<T> {
+  return new InjectionToken<Signal<State<T>>>(`Ngp${description}StateToken`);
+}
+
+/**
+ * Create a new provider for the state. It first tries to inject the state from the parent injector,
+ * as this allows for the state to be hoisted to a higher level in the component tree. This can
+ * be useful to avoid issues where the injector can't be shared in some cases when ng-content is used.
+ * @param token The token for the state
+ */
+export function createStateProvider<T>(token: ProviderToken<T>): () => FactoryProvider {
+  return () => ({
+    provide: token,
+    useFactory: () => inject(token, { optional: true, skipSelf: true }) ?? signal({}),
+  });
+}
+
+type CreateStateInjectorOptions = {
+  /**
+   * Whether the state may not be immediately available. This can happen when the child is instantiated before the parent.
+   */
+  deferred?: boolean;
+};
+
+/**
+ * Create a new state injector for the state.
+ * @param token The token for the state
+ */
+export function createStateInjector<T>(
+  token: ProviderToken<State<T>>,
+  options: { deferred: true },
+): <U = T>() => Signal<State<U> | undefined>;
+export function createStateInjector<T>(
+  token: ProviderToken<State<T>>,
+  options?: CreateStateInjectorOptions,
+): <U = T>() => Signal<State<U>>;
+export function createStateInjector<T>(
+  token: ProviderToken<State<T>>,
+  options: CreateStateInjectorOptions = {},
+): <U = T>() => Signal<State<U> | undefined> {
+  return <U = T>() => {
+    const value = inject(token) as Signal<State<U> | undefined>;
+
+    if (options.deferred) {
+      return computed(() =>
+        Object.keys(value() ?? {}).length === 0 ? undefined : value(),
+      ) as Signal<State<U> | undefined>;
+    }
+
+    return value as Signal<State<U>>;
+  };
+}
+
+/**
+ * Convert the original state object into a writable state object.
+ * @param token The token for the state
+ */
+export function createState(token: ProviderToken<WritableSignal<State<unknown>>>) {
+  return <U>(state: U): State<U> => {
+    const internalState = inject(token);
+
+    internalState.update(obj => {
+      // Iterating over properties
+      for (const key in state) {
+        const value = state[key as keyof U];
+
+        // @ts-ignore
+        obj[key] = isSignal(value) ? linkedSignal(() => value()) : value;
+      }
+
+      // Iterating over prototype methods
+      const prototype = Object.getPrototypeOf(state);
+
+      for (const key of Object.getOwnPropertyNames(prototype)) {
+        (obj as Record<string, unknown>)[key] = prototype[key as keyof U].bind(state);
+      }
+
+      return { ...obj };
+    });
+
+    return internalState() as unknown as State<U>;
+  };
+}
---
+++ packages/state/README.md
@@ -0,0 +1,19 @@
+# Angular Primitives State
+
+Angular Primitives State is a low-level state library with a focus on developer experience.
+
+## Installation
+
+```bash
+npm install @ng-primitives/state
+```
+
+This package is auto-generated from `ng-primitives/state` and the source should be modified there.
+
+## Contributing
+
+We welcome contributions from the community. Please refer to our [contributing guidelines](CONTRIBUTING.md) for more information.
+
+## License
+
+Angular Primitives is licensed under the [Apache License 2.0](LICENSE).
