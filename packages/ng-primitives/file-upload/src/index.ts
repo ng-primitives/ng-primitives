f353b62c9ec05245670fbf31517811b999f616b5
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2025-04-14 22:41:16 +0100

    feat(file-upload): dropzone (#187)

--- packages/ng-primitives/file-upload/src/index.ts
+++ packages/ng-primitives/file-upload/src/index.ts
@@ -1,2 +1,7 @@
+export { NgpFileDropzone } from './file-dropzone/file-dropzone';
+export {
+  injectFileDropzoneState,
+  provideFileDropzoneState,
+} from './file-dropzone/file-dropzone-state';
 export { NgpFileUpload } from './file-upload/file-upload';
 export { injectFileUploadState, provideFileUploadState } from './file-upload/file-upload-state';
