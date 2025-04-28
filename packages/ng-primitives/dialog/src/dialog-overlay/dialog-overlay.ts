438ab9b0b830e8aea5ec7c9bf11ca4711afe7456
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2025-04-28 22:22:26 +0100

    feat(dialog): allow backdrop click to be disabled (#215)

--- packages/ng-primitives/dialog/src/dialog-overlay/dialog-overlay.ts
+++ packages/ng-primitives/dialog/src/dialog-overlay/dialog-overlay.ts
@@ -1,4 +1,5 @@
-import { Directive, HostListener } from '@angular/core';
+import { BooleanInput } from '@angular/cdk/coercion';
+import { booleanAttribute, Directive, HostListener, input } from '@angular/core';
 import { NgpExitAnimation } from 'ng-primitives/internal';
 import { injectDialogRef } from '../dialog/dialog-ref';
 
@@ -11,8 +12,19 @@ export class NgpDialogOverlay {
   /** Access the dialog ref. */
   private readonly dialogRef = injectDialogRef();
 
+  /**
+   * Whether the dialog should close on backdrop click.
+   * @default `true`
+   */
+  readonly closeOnClick = input<boolean, BooleanInput>(true, {
+    alias: 'ngpDialogOverlayCloseOnClick',
+    transform: booleanAttribute,
+  });
+
   @HostListener('click')
   protected close(): void {
-    this.dialogRef.close();
+    if (this.closeOnClick()) {
+      this.dialogRef.close();
+    }
   }
 }
