a1337b1075669942b3fd532db4329317f470ac64
Ashley Hunter <20795331+ashley-hunter@users.noreply.github.com>
2025-05-28 21:47:24 +0100

    adding tests

--- packages/ng-primitives/input/src/input/input.spec.ts
+++ packages/ng-primitives/input/src/input/input.spec.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { fireEvent, render } from '@testing-library/angular';
 import { NgpInput } from './input';
 
@@ -23,5 +24,29 @@ describe('NgpInput', () => {
     expect(input).toHaveAttribute('data-press');
     fireEvent.pointerUp(input);
     expect(input).not.toHaveAttribute('data-press');
+  });
+
+  it('should add data attributes for form control status when ngModel is used', async () => {
+    const { getByTestId } = await render(
+      `<input ngpInput data-testid="input" [(ngModel)]="value" />`,
+      {
+        imports: [NgpInput, FormsModule],
+        componentProperties: { value: '' },
+      },
+    );
+
+    const input = getByTestId('input');
+    expect(input).toHaveAttribute('data-valid');
+    expect(input).not.toHaveAttribute('data-invalid');
+    expect(input).toHaveAttribute('data-pristine');
+    expect(input).not.toHaveAttribute('data-dirty');
+    expect(input).not.toHaveAttribute('data-touched');
+
+    fireEvent.focus(input);
+    fireEvent.input(input, { target: { value: '' } });
+    fireEvent.blur(input);
+
+    expect(input).not.toHaveAttribute('data-pristine');
+    expect(input).toHaveAttribute('data-dirty');
   });
 });
