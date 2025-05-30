290ae81ab366045de52b6c2a2350dc5eaf003fa2
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2025-05-30 21:27:31 +0100

    fix(menu): resolve scroll blocking not working (#270)

--- packages/ng-primitives/portal/src/index.ts
+++ packages/ng-primitives/portal/src/index.ts
@@ -8,3 +8,4 @@ export {
 } from './overlay';
 export { injectOverlayContext, provideOverlayContext } from './overlay-token';
 export { createPortal, NgpComponentPortal, NgpPortal, NgpTemplatePortal } from './portal';
+export { BlockScrollStrategy, NoopScrollStrategy, ScrollStrategy } from './scroll-strategy';
