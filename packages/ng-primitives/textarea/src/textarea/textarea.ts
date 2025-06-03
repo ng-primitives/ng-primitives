1b6a8973f1aafdc92e9d162766f01bd1b7787e22
Ashley Hunter <20795331+ashley-hunter@users.noreply.github.com>
2025-06-03 22:50:01 +0100

    fixing other controls

--- packages/ng-primitives/textarea/src/textarea/textarea.ts
+++ packages/ng-primitives/textarea/src/textarea/textarea.ts
@@ -2,14 +2,24 @@ import { BooleanInput } from '@angular/cdk/coercion';
 import { booleanAttribute, Directive, input } from '@angular/core';
 import { setupFormControl } from 'ng-primitives/form-field';
 import { setupInteractions } from 'ng-primitives/internal';
+import { uniqueId } from 'ng-primitives/utils';
 import { provideTextareaState, textareaState } from './textarea-state';
 
 @Directive({
   selector: '[ngpTextarea]',
   exportAs: 'ngpTextarea',
   providers: [provideTextareaState()],
+  host: {
+    '[id]': 'id()',
+    '[attr.disabled]': 'disabled() ? "" : null',
+  },
 })
 export class NgpTextarea {
+  /**
+   * The id of the textarea. If not provided, a unique id will be generated.
+   */
+  readonly id = input<string>(uniqueId('ngp-textarea'));
+
   /**
    * Whether the element is disabled.
    */
@@ -29,6 +39,6 @@ export class NgpTextarea {
       focus: true,
       disabled: this.state.disabled,
     });
-    setupFormControl({ disabled: this.state.disabled });
+    setupFormControl({ id: this.state.id, disabled: this.state.disabled });
   }
 }
