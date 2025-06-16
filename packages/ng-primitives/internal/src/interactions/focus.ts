3bb00724a32c488ed98f9fa4d373e91fbfcb1740
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2025-06-16 15:20:44 +0100

    fix(combobox): adding interaction states

--- packages/ng-primitives/internal/src/interactions/focus.ts
+++ packages/ng-primitives/internal/src/interactions/focus.ts
@@ -1,8 +1,10 @@
+import { FocusMonitor } from '@angular/cdk/a11y';
 import { ElementRef, Renderer2, Signal, inject, signal } from '@angular/core';
-import { injectDisposables } from 'ng-primitives/utils';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 export interface NgpFocusOptions {
   disabled?: Signal<boolean>;
+  focusWithin?: boolean;
   focus?: () => void;
   blur?: () => void;
 }
@@ -14,6 +16,7 @@ export interface NgpFocusState {
 export function setupFocus({
   focus,
   blur,
+  focusWithin = false,
   disabled = signal(false),
 }: NgpFocusOptions): NgpFocusState {
   /**
@@ -22,9 +25,9 @@ export function setupFocus({
   const elementRef = inject<ElementRef<HTMLElement>>(ElementRef);
 
   /**
-   * Access the disposables helper.
+   * Access the focus monitor.
    */
-  const disposables = injectDisposables();
+  const focusMonitor = inject(FocusMonitor);
 
   /**
    * Access the renderer.
@@ -36,48 +39,27 @@ export function setupFocus({
    */
   const isFocused = signal<boolean>(false);
 
-  // setup event listeners
-  disposables.addEventListener(elementRef.nativeElement, 'focus', onFocus);
-  disposables.addEventListener(elementRef.nativeElement, 'blur', onBlur);
+  focusMonitor
+    .monitor(elementRef, focusWithin)
+    .pipe(takeUntilDestroyed())
+    .subscribe(focusOrigin => {
+      if (disabled()) {
+        return;
+      }
 
-  /**
-   * Listen for focus events.
-   * @param event
-   */
-  function onFocus(event: FocusEvent): void {
-    if (disabled()) {
-      return;
-    }
+      isFocused.set(focusOrigin !== null);
+      if (focusOrigin !== null) {
+        if (focus) {
+          focus();
+        }
+        renderer.setAttribute(elementRef.nativeElement, 'data-focus', '');
+      } else {
+        if (blur) {
+          blur();
+        }
+        renderer.removeAttribute(elementRef.nativeElement, 'data-focus');
+      }
+    });
 
-    const ownerDocument = (event.target as HTMLElement)?.ownerDocument ?? document;
-
-    // ensure this element is still focused
-    if (ownerDocument.activeElement === event.target && event.currentTarget === event.target) {
-      focus?.();
-      isFocused.set(true);
-      // set the data-focus attribute
-      renderer.setAttribute(elementRef.nativeElement, 'data-focus', '');
-    }
-  }
-
-  /**
-   * Listen for blur events.
-   * @param event
-   */
-  function onBlur(event: FocusEvent): void {
-    if (disabled()) {
-      return;
-    }
-
-    if (event.currentTarget === event.target) {
-      blur?.();
-      isFocused.set(false);
-      // remove the data-focus attribute
-      renderer.removeAttribute(elementRef.nativeElement, 'data-focus');
-    }
-  }
-
-  return {
-    isFocused,
-  };
+  return { isFocused };
 }
