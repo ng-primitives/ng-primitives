7ddc84c06d2e8217d8efc25bd2daee511ac820f3
adriankopytko <15912513+adriankopytko@users.noreply.github.com>
2025-06-19 05:26:31 +0200

    fix(utils): sync data attributes in controlStatus on initialization and every following control event, even if control becomes available later

--- packages/ng-primitives/utils/src/forms/status.ts
+++ packages/ng-primitives/utils/src/forms/status.ts
@@ -1,4 +1,4 @@
-import { Signal, inject, signal } from '@angular/core';
+import { DestroyRef, Signal, WritableSignal, afterNextRender, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { NgControl } from '@angular/forms';
 
@@ -12,6 +12,39 @@ export interface NgpControlStatus {
   disabled: boolean | null;
 }
 
+function setStatusSignal(
+  control: NgControl | null,
+  status: WritableSignal<NgpControlStatus>,
+): void {
+  if (!control?.control) {
+    return;
+  }
+
+  status.set({
+    valid: control?.control?.valid ?? null,
+    invalid: control?.control?.invalid ?? null,
+    pristine: control?.control?.pristine ?? null,
+    dirty: control?.control?.dirty ?? null,
+    touched: control?.control?.touched ?? null,
+    pending: control?.control?.pending ?? null,
+    disabled: control?.control?.disabled ?? null,
+  });
+}
+
+function subscribeToControlStatus(
+  control: NgControl | null,
+  status: WritableSignal<NgpControlStatus>,
+  destroyRef?: DestroyRef,
+): void {
+  if (!control?.control) {
+    return;
+  }
+
+  control.control.events
+    .pipe(takeUntilDestroyed(destroyRef))
+    .subscribe(() => setStatusSignal(control, status));
+}
+
 /**
  * A utility function to get the status of an Angular form control as a reactive signal.
  * This function injects the NgControl and returns a signal that reflects the control's status.
@@ -19,6 +52,7 @@ export interface NgpControlStatus {
  */
 export function controlStatus(): Signal<NgpControlStatus> {
   const control = inject(NgControl, { optional: true });
+  const destroyRef = inject(DestroyRef);
 
   const status = signal<NgpControlStatus>({
     valid: null,
@@ -32,20 +66,22 @@ export function controlStatus(): Signal<NgpControlStatus> {
 
   // Fallback if control is not yet available
   if (!control?.control) {
+    // There is still a chance that the control will be available i.e. after executing OnInit lifecycle hook
+    // in `formControlName` directive, so we set up an effect to subscribe to the control status
+    afterNextRender({
+      write: () => {
+        // If control is still not available, we do nothing, otherwise we subscribe to the control status
+        if (control?.control) {
+          subscribeToControlStatus(control, status, destroyRef);
+          // We re-set the status to ensure it reflects the current state on initialization
+          setStatusSignal(control, status);
+        }
+      },
+    });
     return status;
   }
 
-  control.control.statusChanges.pipe(takeUntilDestroyed()).subscribe(() => {
-    status.set({
-      valid: control?.control?.valid ?? null,
-      invalid: control?.control?.invalid ?? null,
-      pristine: control?.control?.pristine ?? null,
-      dirty: control?.control?.dirty ?? null,
-      touched: control?.control?.touched ?? null,
-      pending: control?.control?.pending ?? null,
-      disabled: control?.control?.disabled ?? null,
-    });
-  });
+  subscribeToControlStatus(control, status);
 
   return status;
 }
