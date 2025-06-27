ea30eabfcb505f8cc9f63f6c59e5af9e1a8b3ae3
kedevked <edoh.dev@gmail.com>
2025-06-27 09:19:43 +0200

    feat: remove css variables

--- apps/documentation/src/app/examples/avatar/avatar.tailwind.example.ts
+++ apps/documentation/src/app/examples/avatar/avatar.tailwind.example.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NgpAvatar, NgpAvatarFallback, NgpAvatarImage } from 'ng-primitives/avatar';
 
+// class="relative inline-flex size-12 items-center justify-center overflow-hidden rounded-full border-2 border-white bg-sky-200 before:absolute before:inset-0 before:rounded-full before:border before:border-black/10 dark:border-zinc-800 dark:bg-sky-900"
 @Component({
   selector: 'app-avatar',
   standalone: true,
   imports: [NgpAvatar, NgpAvatarImage, NgpAvatarFallback],
   template: `
     <span
-      class="relative inline-flex h-12 w-12 items-center justify-center overflow-hidden rounded-full border-2 border-[var(--ngp-avatar-border)] bg-[var(--ngp-avatar-background)] ring-1 ring-inset ring-black/10"
+      class="relative inline-flex size-12 items-center justify-center overflow-hidden rounded-full border-2 border-white bg-sky-200 before:absolute before:inset-0 before:rounded-full before:border before:border-black/10 dark:border-zinc-700 dark:bg-sky-900"
       ngpAvatar
     >
       <img
@@ -16,7 +17,7 @@ import { NgpAvatar, NgpAvatarFallback, NgpAvatarImage } from 'ng-primitives/avat
         src="https://angularprimitives.com/assets/avatar.png"
         alt="Profile Image"
       />
-      <span class="font-medium text-[var(--ngp-text-emphasis)]" ngpAvatarFallback>NG</span>
+      <span class="font-medium text-sky-900 dark:text-sky-200" ngpAvatarFallback>NG</span>
     </span>
   `,
 })
