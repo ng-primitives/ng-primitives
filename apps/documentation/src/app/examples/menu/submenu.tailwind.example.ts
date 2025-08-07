6446d3a099a7453f556be64f38e9577276d8c16f
kedevked <edoh.dev@gmail.com>
2025-08-08 00:48:53 +0200

    feat: add submenu tailwind example

---
+++ apps/documentation/src/app/examples/menu/submenu.tailwind.example.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroChevronRightMini } from '@ng-icons/heroicons/mini';
+import { NgpButton } from 'ng-primitives/button';
+import { NgpMenu, NgpMenuItem, NgpMenuTrigger, NgpSubmenuTrigger } from 'ng-primitives/menu';
+
+@Component({
+  selector: 'app-menu-submenu-tailwind',
+  standalone: true,
+  imports: [NgpButton, NgpMenu, NgpMenuTrigger, NgpMenuItem, NgpSubmenuTrigger, NgIcon],
+  providers: [provideIcons({ heroChevronRightMini })],
+  template: `
+    <button
+      class="h-10 rounded-lg border-none bg-white px-4 font-medium text-gray-900 shadow outline-none ring-1 ring-black/5 transition-colors duration-300 hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-blue-500 active:bg-gray-200 dark:bg-transparent dark:text-gray-100 dark:ring-white/10 dark:hover:bg-white/10 dark:active:bg-white/20"
+      [ngpMenuTrigger]="menu"
+      ngpButton
+    >
+      Open Menu
+    </button>
+
+    <ng-template #menu>
+      <div
+        class="animate-in fade-in-0 zoom-in-95 fixed flex w-max origin-[var(--ngp-menu-transform-origin)] flex-col rounded-lg border border-gray-200 bg-white p-1 shadow-lg dark:border-gray-700 dark:bg-black"
+        ngpMenu
+      >
+        <button
+          class="flex min-w-[120px] cursor-pointer items-center justify-between rounded border-none bg-transparent px-[14px] py-[6px] text-left text-[14px] font-normal outline-none transition-colors hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-blue-500 active:bg-gray-200 dark:text-gray-100 dark:hover:bg-white/10 dark:active:bg-white/20"
+          ngpMenuItem
+        >
+          Item 1
+        </button>
+        <button
+          class="flex min-w-[120px] cursor-pointer items-center justify-between rounded border-none bg-transparent px-[14px] py-[6px] text-left text-[14px] font-normal outline-none transition-colors hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-blue-500 active:bg-gray-200 dark:text-gray-100 dark:hover:bg-white/10 dark:active:bg-white/20"
+          ngpMenuItem
+        >
+          Item 2
+        </button>
+        <button
+          class="flex min-w-[120px] cursor-pointer items-center justify-between rounded border-none bg-transparent px-[14px] py-[6px] text-left text-[14px] font-normal outline-none transition-colors hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-blue-500 active:bg-gray-200 dark:text-gray-100 dark:hover:bg-white/10 dark:active:bg-white/20"
+          [ngpSubmenuTrigger]="submenu"
+          ngpMenuItem
+        >
+          Item 3
+          <ng-icon name="heroChevronRightMini" />
+        </button>
+      </div>
+    </ng-template>
+
+    <ng-template #submenu>
+      <div
+        class="animate-in fade-in-0 zoom-in-95 fixed flex w-max origin-[var(--ngp-menu-transform-origin)] flex-col rounded-lg border border-gray-200 bg-white p-1 shadow-lg dark:border-gray-700 dark:bg-black"
+        ngpMenu
+      >
+        <button
+          class="flex min-w-[120px] cursor-pointer items-center justify-between rounded border-none bg-transparent px-[14px] py-[6px] text-left text-[14px] font-normal outline-none transition-colors hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-blue-500 active:bg-gray-200 dark:text-gray-100 dark:hover:bg-white/10 dark:active:bg-white/20"
+          ngpMenuItem
+        >
+          Item 1
+        </button>
+        <button
+          class="flex min-w-[120px] cursor-pointer items-center justify-between rounded border-none bg-transparent px-[14px] py-[6px] text-left text-[14px] font-normal outline-none transition-colors hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-blue-500 active:bg-gray-200 dark:text-gray-100 dark:hover:bg-white/10 dark:active:bg-white/20"
+          ngpMenuItem
+        >
+          Item 2
+        </button>
+        <button
+          class="flex min-w-[120px] cursor-pointer items-center justify-between rounded border-none bg-transparent px-[14px] py-[6px] text-left text-[14px] font-normal outline-none transition-colors hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-blue-500 active:bg-gray-200 dark:text-gray-100 dark:hover:bg-white/10 dark:active:bg-white/20"
+          ngpMenuItem
+        >
+          Item 3
+        </button>
+      </div>
+    </ng-template>
+  `,
+})
+export default class MenuSubmenuTailwindExample {}
