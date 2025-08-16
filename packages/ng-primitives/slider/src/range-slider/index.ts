9a7c369340015a0bedc2d1fb333e582a452fb5ea
Ramon Martinez <ramon.martinez@tisal.cl>
2025-08-16 15:36:43 -0400

    fix: delete index of range slider

--- packages/ng-primitives/slider/src/range-slider/index.ts
+++
@@ -1,5 +0,0 @@
-export { NgpRangeSliderRange } from './range-slider-range/range-slider-range';
-export { NgpRangeSliderThumb } from './range-slider-thumb/range-slider-thumb';
-export { NgpRangeSliderTrack } from './range-slider-track/range-slider-track';
-export { NgpRangeSlider } from './range-slider/range-slider';
-export { injectRangeSliderState, provideRangeSliderState } from './range-slider/range-slider-state';
