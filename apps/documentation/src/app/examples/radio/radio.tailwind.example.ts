345570614d82e82df372fa788a065f937b43207c
kedevked <edoh.dev@gmail.com>
2025-08-18 22:09:44 +0200

    feat: add radio tailwind example (#429)
    * feat: add radio tailwind example
    
    * feat: add shadow around tailwind radio items
    
    * feat: improve background colors
    
    * feat: match ngpRadioIndicator styling in dark mode for tailwind
    
    * Update radio.tailwind.example.ts
    
    ---------
    
    Co-authored-by: Ashley Hunter <ashley.hunter@hotmail.co.uk>

---
+++ apps/documentation/src/app/examples/radio/radio.tailwind.example.ts
@@ -0,0 +1,83 @@
+import { Component, signal } from '@angular/core';
+import { NgpRadioGroup, NgpRadioIndicator, NgpRadioItem } from 'ng-primitives/radio';
+
+@Component({
+  selector: 'app-radio-tailwind',
+  imports: [NgpRadioGroup, NgpRadioItem, NgpRadioIndicator],
+  template: `
+    <div
+      class="flex flex-col gap-4"
+      [(ngpRadioGroupValue)]="plan"
+      ngpRadioGroup
+      ngpRadioGroupOrientation="vertical"
+    >
+      <div
+        class="group grid h-[66px] cursor-pointer grid-cols-[auto,1fr] grid-rows-2 gap-x-[0.625rem] gap-y-[0.125rem] rounded-lg bg-white px-4 py-3 text-black shadow outline-none ring-1 ring-black/5 transition-colors duration-200 data-[checked]:bg-black data-[checked]:text-white data-[focus-visible]:outline-2 data-[focus-visible]:outline-offset-2 data-[focus-visible]:outline-blue-500 dark:bg-black dark:text-white dark:shadow dark:ring-white/10 dark:data-[checked]:bg-white dark:data-[checked]:text-black [&[data-hover]:not([data-checked])]:bg-gray-100 dark:[&[data-hover]:not([data-checked])]:bg-zinc-800 [&[data-press]:not([data-checked])]:bg-gray-200 dark:[&[data-press]:not([data-checked])]:bg-zinc-700"
+        ngpRadioItem
+        ngpRadioItemValue="indie"
+      >
+        <div
+          class="inline-flex h-4 w-4 items-center justify-center self-center rounded-full border border-gray-300 bg-white transition-colors duration-200 data-[checked]:border-gray-900 data-[checked]:bg-black dark:border-zinc-700 dark:bg-transparent dark:data-[checked]:border-black dark:data-[checked]:bg-white"
+          ngpRadioIndicator
+        >
+          <span
+            class="h-2 w-2 rounded-full bg-white transition-colors group-data-[checked]:bg-white dark:bg-transparent dark:group-data-[checked]:bg-black"
+          ></span>
+        </div>
+        <p class="title col-start-2 row-start-1 m-0 text-sm font-medium leading-5">Indie Plan</p>
+        <p
+          class="description col-start-2 row-start-2 m-0 text-xs leading-4 text-zinc-600 group-data-[checked]:text-white dark:text-zinc-400 dark:group-data-[checked]:text-black"
+        >
+          For those who want to are just starting out
+        </p>
+      </div>
+
+      <div
+        class="group grid h-[66px] cursor-pointer grid-cols-[auto,1fr] grid-rows-2 gap-x-[0.625rem] gap-y-[0.125rem] rounded-lg bg-white px-4 py-3 text-black shadow outline-none ring-1 ring-black/5 transition-colors duration-200 data-[checked]:bg-black data-[checked]:text-white data-[focus-visible]:outline-2 data-[focus-visible]:outline-offset-2 data-[focus-visible]:outline-blue-500 dark:bg-black dark:text-white dark:shadow dark:ring-white/10 dark:data-[checked]:bg-white dark:data-[checked]:text-black [&[data-hover]:not([data-checked])]:bg-gray-100 dark:[&[data-hover]:not([data-checked])]:bg-zinc-800 [&[data-press]:not([data-checked])]:bg-gray-200 dark:[&[data-press]:not([data-checked])]:bg-zinc-700"
+        ngpRadioItem
+        ngpRadioItemValue="growth"
+      >
+        <div
+          class="inline-flex h-4 w-4 items-center justify-center self-center rounded-full border border-gray-300 bg-white transition-colors duration-200 data-[checked]:border-gray-900 data-[checked]:bg-black dark:border-zinc-700 dark:bg-transparent dark:data-[checked]:border-black dark:data-[checked]:bg-white"
+          ngpRadioIndicator
+        >
+          <span
+            class="h-2 w-2 rounded-full bg-transparent transition-colors group-data-[checked]:bg-white dark:group-data-[checked]:bg-black"
+          ></span>
+        </div>
+        <p class="title col-start-2 row-start-1 m-0 text-sm font-medium leading-5">Growth Plan</p>
+        <p
+          class="description col-start-2 row-start-2 m-0 text-xs leading-4 text-zinc-600 group-data-[checked]:text-white dark:text-zinc-400 dark:group-data-[checked]:text-black"
+        >
+          For those who want to grow their business
+        </p>
+      </div>
+
+      <div
+        class="group grid h-[66px] cursor-pointer grid-cols-[auto,1fr] grid-rows-2 gap-x-[0.625rem] gap-y-[0.125rem] rounded-lg bg-white px-4 py-3 text-black shadow outline-none ring-1 ring-black/5 transition-colors duration-200 data-[checked]:bg-black data-[checked]:text-white data-[focus-visible]:outline-2 data-[focus-visible]:outline-offset-2 data-[focus-visible]:outline-blue-500 dark:bg-black dark:text-white dark:shadow dark:ring-white/10 dark:data-[checked]:bg-white dark:data-[checked]:text-black [&[data-hover]:not([data-checked])]:bg-gray-100 dark:[&[data-hover]:not([data-checked])]:bg-zinc-800 [&[data-press]:not([data-checked])]:bg-gray-200 dark:[&[data-press]:not([data-checked])]:bg-zinc-700"
+        ngpRadioItem
+        ngpRadioItemValue="unicorn"
+      >
+        <div
+          class="inline-flex h-4 w-4 items-center justify-center self-center rounded-full border border-gray-300 bg-white transition-colors duration-200 data-[checked]:border-gray-900 data-[checked]:bg-black dark:border-zinc-700 dark:bg-transparent dark:data-[checked]:border-black dark:data-[checked]:bg-white"
+          ngpRadioIndicator
+        >
+          <span
+            class="h-2 w-2 rounded-full bg-transparent transition-colors group-data-[checked]:bg-white dark:group-data-[checked]:bg-black"
+          ></span>
+        </div>
+        <p class="title col-start-2 row-start-1 m-0 text-sm font-medium leading-5">Unicorn Plan</p>
+        <p
+          class="description col-start-2 row-start-2 m-0 text-xs leading-4 text-zinc-600 group-data-[checked]:text-white dark:text-zinc-400 dark:group-data-[checked]:text-black"
+        >
+          For those who are going to the moon
+        </p>
+      </div>
+    </div>
+  `,
+})
+export default class RadioTailwindExample {
+  readonly plan = signal<Plan>('indie');
+}
+
+type Plan = 'indie' | 'growth' | 'unicorn';
