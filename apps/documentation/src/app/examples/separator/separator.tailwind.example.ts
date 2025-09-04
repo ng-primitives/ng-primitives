488b7449b35da4f09e88bb5a4fcd2b2885fb3587
kedevked <edoh.dev@gmail.com>
2025-09-04 21:59:46 +0200

    feat: add separator tailwind example (#451)

---
+++ apps/documentation/src/app/examples/separator/separator.tailwind.example.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NgpSeparator } from 'ng-primitives/separator';
+
+@Component({
+  selector: 'app-separator-tailwind',
+  imports: [NgpSeparator],
+  template: `
+    <div
+      class="flex max-w-[300px] flex-col gap-4 rounded-lg border border-neutral-200 bg-white p-4 dark:border-neutral-800 dark:bg-transparent"
+    >
+      <p class="m-0 text-black dark:text-white">
+        The separator primitive can be used to separate content in a layout.
+      </p>
+      <div class="h-[1px] w-full bg-neutral-200 dark:bg-neutral-800" ngpSeparator></div>
+      <p class="m-0 text-black dark:text-white">
+        It supports both horizontal and vertical orientations.
+      </p>
+    </div>
+  `,
+})
+export default class SeparatorTailwindExample {}
