78c2ff2dfb7e9d3283ee9ac5c3acb003eacb64d9
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2025-09-23 20:59:52 +0100

    small style fix

--- apps/documentation/src/app/examples/switch/switch-form-field.tailwind.example.ts
+++ apps/documentation/src/app/examples/switch/switch-form-field.tailwind.example.ts
@@ -10,11 +10,11 @@ import { NgpSwitch, NgpSwitchThumb } from 'ng-primitives/switch';
     <div class="flex items-center gap-4" ngpFormField>
       <label class="font-medium text-neutral-900 dark:text-neutral-50" ngpLabel>Mobile Data</label>
       <button
-        class="relative h-6 w-10 rounded-full border border-neutral-300 bg-neutral-200 p-0 outline-none transition duration-150 ease-in-out data-[checked]:border-blue-600 data-[checked]:bg-blue-100 data-[focus-visible]:ring-2 data-[focus-visible]:ring-blue-500 dark:border-neutral-700 dark:bg-neutral-800 dark:data-[checked]:border-blue-600"
+        class="relative h-6 w-10 rounded-full bg-neutral-300 p-0 outline-none ring-1 ring-inset ring-black/10 transition duration-150 ease-in-out data-[checked]:bg-blue-100 data-[focus-visible]:ring-2 data-[checked]:ring-blue-600 data-[focus-visible]:ring-blue-500 dark:border-neutral-700 dark:bg-neutral-800 dark:data-[checked]:ring-blue-600"
         ngpSwitch
       >
         <span
-          class="block h-5 w-5 translate-x-0.5 transform rounded-full bg-white shadow-sm ring-0 transition-transform duration-150 ease-in-out data-[checked]:translate-x-[1.125rem]"
+          class="block h-5 w-5 translate-x-[2px] transform rounded-full bg-white shadow-sm ring-0 transition-transform duration-150 ease-in-out data-[checked]:translate-x-[18px]"
           ngpSwitchThumb
         ></span>
       </button>
