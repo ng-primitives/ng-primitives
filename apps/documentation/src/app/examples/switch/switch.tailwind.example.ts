1312dc088e22cbf0560dbe333391c68045bda404
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2025-09-23 21:01:49 +0100

    dark theme fix

--- apps/documentation/src/app/examples/switch/switch.tailwind.example.ts
+++ apps/documentation/src/app/examples/switch/switch.tailwind.example.ts
@@ -7,7 +7,7 @@ import { NgpSwitch, NgpSwitchThumb } from 'ng-primitives/switch';
   imports: [NgpSwitch, NgpSwitchThumb],
   template: `
     <button
-      class="relative h-6 w-10 rounded-full bg-neutral-300 p-0 outline-none ring-1 ring-inset ring-black/10 transition duration-150 ease-in-out data-[checked]:bg-blue-100 data-[focus-visible]:ring-2 data-[checked]:ring-blue-600 data-[focus-visible]:ring-blue-500 dark:border-neutral-700 dark:bg-neutral-800 dark:data-[checked]:ring-blue-600"
+      class="relative h-6 w-10 rounded-full bg-neutral-300 p-0 outline-none ring-1 ring-inset ring-black/10 transition duration-150 ease-in-out data-[checked]:bg-blue-100 data-[data-checked]:bg-blue-900 data-[focus-visible]:ring-2 data-[checked]:ring-blue-600 data-[focus-visible]:ring-blue-500 dark:bg-neutral-800 dark:data-[checked]:ring-blue-600"
       ngpSwitch
     >
       <span
