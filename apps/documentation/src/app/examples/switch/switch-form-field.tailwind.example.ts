0677ff7d85228ba8610702773f44704fbf0c4b40
kedevked <edoh.dev@gmail.com>
2025-09-23 22:03:21 +0200

    feat: add switch tailwind examples (#473)
    * feat: add switch tailwind examples
    
    * small style fix
    
    * dark theme fix
    
    ---------
    
    Co-authored-by: Ashley Hunter <ashley.hunter@hotmail.co.uk>

---
+++ apps/documentation/src/app/examples/switch/switch-form-field.tailwind.example.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NgpFormField, NgpLabel } from 'ng-primitives/form-field';
+import { NgpSwitch, NgpSwitchThumb } from 'ng-primitives/switch';
+
+@Component({
+  selector: 'app-switch-form-field-tailwind',
+  standalone: true,
+  imports: [NgpSwitch, NgpSwitchThumb, NgpFormField, NgpLabel],
+  template: `
+    <div class="flex items-center gap-4" ngpFormField>
+      <label class="font-medium text-neutral-900 dark:text-neutral-50" ngpLabel>Mobile Data</label>
+      <button
+        class="relative h-6 w-10 rounded-full bg-neutral-300 p-0 outline-none ring-1 ring-inset ring-black/10 transition duration-150 ease-in-out data-[checked]:bg-blue-100 data-[data-checked]:bg-blue-900 data-[focus-visible]:ring-2 data-[checked]:ring-blue-600 data-[focus-visible]:ring-blue-500 dark:bg-neutral-800 dark:data-[checked]:ring-blue-600"
+        ngpSwitch
+      >
+        <span
+          class="block h-5 w-5 translate-x-[2px] transform rounded-full bg-white shadow-sm ring-0 transition-transform duration-150 ease-in-out data-[checked]:translate-x-[18px]"
+          ngpSwitchThumb
+        ></span>
+      </button>
+    </div>
+  `,
+})
+export default class SwitchFormFieldTailwindExample {}
