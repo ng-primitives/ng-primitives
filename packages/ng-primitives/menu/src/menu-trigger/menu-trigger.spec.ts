c46b88b64bdf714edaf6b307583bef74a6a1248c
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2026-01-12 14:42:57 +0000

    Update menu-trigger.spec.ts

--- packages/ng-primitives/menu/src/menu-trigger/menu-trigger.spec.ts
+++ packages/ng-primitives/menu/src/menu-trigger/menu-trigger.spec.ts
@@ -1,11 +1,29 @@
-import { render } from '@testing-library/angular';
-import { NgpMenuTrigger } from './menu-trigger';
+import { fakeAsync, flush } from '@angular/core/testing';
+import { fireEvent, render, screen } from '@testing-library/angular';
+import { NgpMenu, NgpMenuItem, NgpMenuTrigger } from '../index';
 
 describe('NgpMenuTrigger', () => {
-  it('should initialise correctly', async () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const container = await render(`<div ngpMenuTrigger></div>`, {
-      imports: [NgpMenuTrigger],
-    });
-  });
+  it('should open a menu on click', fakeAsync(async () => {
+    const { getByText } = await render(
+      `<button [ngpMenuTrigger]="menu">Open Menu</button>
+
+    <ng-template #menu>
+      <div ngpMenu data-testid="ngp-menu">
+        <button ngpMenuItem>Item 1</button>
+        <button ngpMenuItem>Item 2</button>
+        <button ngpMenuItem>Item 3</button>
+      </div>
+    </ng-template>`,
+      {
+        imports: [NgpMenuTrigger, NgpMenu, NgpMenuItem],
+      },
+    );
+
+    const trigger = getByText('Open Menu');
+    fireEvent.click(trigger);
+    flush();
+
+    const menu = screen.getByTestId('ngp-menu');
+    expect(menu).toBeInTheDocument();
+  }));
 });
