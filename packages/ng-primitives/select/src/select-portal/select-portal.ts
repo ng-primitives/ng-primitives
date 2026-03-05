60eb5febc4d5194cfa7a1da8be27d3c6b31ae348
ZecKa <18116930+zecka@users.noreply.github.com>
2026-03-05 22:52:33 +0100

    refactor(select): register select elements as state instead of class

--- packages/ng-primitives/select/src/select-portal/select-portal.ts
+++ packages/ng-primitives/select/src/select-portal/select-portal.ts
@@ -1,53 +1,12 @@
-import { computed, Directive, OnDestroy } from '@angular/core';
-import { injectSelectState } from '../select/select-state';
+import { Directive } from '@angular/core';
 import { ngpSelectPortal } from './select-portal-state';
 
 @Directive({
   selector: '[ngpSelectPortal]',
   exportAs: 'ngpSelectPortal',
 })
-export class NgpSelectPortal implements OnDestroy {
-  /** Access the select state. */
-  private readonly selectState = injectSelectState();
-
-  /** Access the portal state. */
-  private readonly portalState = ngpSelectPortal({});
-
+export class NgpSelectPortal {
   constructor() {
-    this.selectState().registerPortal(this);
-  }
-
-  ngOnDestroy(): void {
-    this.portalState.overlay()?.destroy();
-  }
-
-  /**
-   * Show the portal.
-   * @internal
-   */
-  show(): Promise<void> {
-    return this.portalState.show();
-  }
-
-  /**
-   * Hide the portal.
-   * @internal
-   */
-  async hide(): Promise<void> {
-    return this.portalState.hide();
-  }
-
-  /**
-   * The overlay that manages the popover
-   * @internal
-   */
-  overlay = computed(() => this.portalState.overlay());
-
-  /**
-   * Detach the portal.
-   * @internal
-   */
-  async detach(): Promise<void> {
-    this.overlay()?.hide();
+    ngpSelectPortal({});
   }
 }
