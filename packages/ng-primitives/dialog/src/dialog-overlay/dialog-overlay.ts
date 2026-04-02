89a7f3deb141e34cf7cee65341d180fc32d04a7e
Lukas Heddendorp <lu.heddendorp@gmail.com>
2026-04-02 10:19:28 +0200

    fix(dialog): guard overlay close against drag selections (#717)

--- packages/ng-primitives/dialog/src/dialog-overlay/dialog-overlay.ts
+++ packages/ng-primitives/dialog/src/dialog-overlay/dialog-overlay.ts
@@ -1,4 +1,4 @@
-import { booleanAttribute, Directive, HostListener, input } from '@angular/core';
+import { booleanAttribute, Directive, input } from '@angular/core';
 import { NgpExitAnimation } from 'ng-primitives/internal';
 import { injectDialogRef } from '../dialog/dialog-ref';
 
@@ -6,9 +6,15 @@ import { injectDialogRef } from '../dialog/dialog-ref';
   selector: '[ngpDialogOverlay]',
   exportAs: 'ngpDialogOverlay',
   hostDirectives: [NgpExitAnimation],
+  host: {
+    '(pointerdown)': 'onPointerDown($event)',
+    '(click)': 'onClick($event)',
+    '(pointercancel)': 'resetPointerOrigin()',
+  },
 })
 export class NgpDialogOverlay {
   private readonly dialogRef = injectDialogRef();
+  private startedPointerDownOnOverlay = false;
 
   /**
    * Whether the dialog should close on backdrop click.
@@ -19,10 +25,25 @@ export class NgpDialogOverlay {
     transform: booleanAttribute,
   });
 
-  @HostListener('click')
-  protected close(): void {
-    if (this.closeOnClick() && !this.dialogRef.disableClose) {
+  protected onPointerDown(event: Event): void {
+    this.startedPointerDownOnOverlay = event.target === event.currentTarget;
+  }
+
+  protected onClick(event: Event): void {
+    const shouldClose =
+      this.startedPointerDownOnOverlay &&
+      event.target === event.currentTarget &&
+      this.closeOnClick() &&
+      !this.dialogRef.disableClose;
+
+    this.resetPointerOrigin();
+
+    if (shouldClose) {
       this.dialogRef.close(undefined, 'mouse');
     }
+  }
+
+  protected resetPointerOrigin(): void {
+    this.startedPointerDownOnOverlay = false;
   }
 }
