055013a96f725b02522906a0cc6ddf488aa908c0
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2026-07-11 22:40:56 +0100

    refactor(dialog): let title/description state own id and teardown
    The title and description directives duplicated the id binding (host [id]
    plus the state's attrBinding) and the teardown (ngOnDestroy plus the state's
    onDestroy). Drop the directive host binding and ngOnDestroy so the state is
    the single owner, matching the root dialog part.

--- packages/ng-primitives/dialog/src/dialog-description/dialog-description.ts
+++ packages/ng-primitives/dialog/src/dialog-description/dialog-description.ts
@@ -1,23 +1,16 @@
-import { Directive, input, OnDestroy } from '@angular/core';
+import { Directive, input } from '@angular/core';
 import { uniqueId } from 'ng-primitives/utils';
 import { ngpDialogDescription } from './dialog-description-state';
 
 @Directive({
   selector: '[ngpDialogDescription]',
   exportAs: 'ngpDialogDescription',
-  host: {
-    '[id]': 'id()',
-  },
 })
-export class NgpDialogDescription implements OnDestroy {
+export class NgpDialogDescription {
   /** The id of the descriptions. */
   readonly id = input<string>(uniqueId('ngp-dialog-description'));
 
   protected readonly state = ngpDialogDescription({
     id: this.id,
   });
-
-  ngOnDestroy(): void {
-    return this.state.destroy();
-  }
 }
--- packages/ng-primitives/dialog/src/dialog-title/dialog-title.ts
+++ packages/ng-primitives/dialog/src/dialog-title/dialog-title.ts
@@ -1,23 +1,16 @@
-import { Directive, input, OnDestroy } from '@angular/core';
+import { Directive, input } from '@angular/core';
 import { uniqueId } from 'ng-primitives/utils';
 import { ngpDialogTitle } from './dialog-title-state';
 
 @Directive({
   selector: '[ngpDialogTitle]',
   exportAs: 'ngpDialogTitle',
-  host: {
-    '[id]': 'id()',
-  },
 })
-export class NgpDialogTitle implements OnDestroy {
+export class NgpDialogTitle {
   /** The id of the title. */
   readonly id = input<string>(uniqueId('ngp-dialog-title'));
 
   protected readonly state = ngpDialogTitle({
     id: this.id,
   });
-
-  ngOnDestroy(): void {
-    return this.state.destroy();
-  }
 }
